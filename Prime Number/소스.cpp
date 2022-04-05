d61ccbf837981295e212c7db6e66eda3e3fc297f
DDunos <jijo0718@gmail.com>
2022-04-05 17:10:53 +0900

    Prime Number

---
+++ Prime Number/소스.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int check[10001] = { false };
+
+class P_Number {
+public:
+	int n;
+	void isPNum() {
+		check[0] = check[1] = true;
+		for (int i = 2; i < sqrt(n); i++) {
+			if (check[i] == false) {
+				for (int j = i + i; j <= n; j += i) {
+					check[j] = true;
+				}
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			if (!check[i]) printf("%d ", i);
+		}
+	}
+	P_Number(int a) { n = a; }
+};
+int main() {
+	int n;
+	cout << "소수 구하는 범위 : ";
+	cin >> n;
+	P_Number a = P_Number(n);
+	a.isPNum();
+	return 0;
+}
